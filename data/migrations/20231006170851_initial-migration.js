0b38c068ab1deae882b827944d178780a37d1ba7
Victoria Blancas <vikki6688@gmail.com>
2023-10-06 15:15:59 -0400

    wip

---
+++ data/migrations/20231006170851_initial-migration.js
@@ -0,0 +1,55 @@
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('recipes', table =>{
+        table.increments('recipe_id')
+        table.string('recipe_ name', 200)
+            .notNullable()
+            .unique()
+    })
+    .createTable('ingredients', table=> {
+        table.increments('ingredient_id')
+        table.string('ingredient_name', 200)
+            .notNullable()
+            .unique()
+        table.string('ingredient_unit', 50)
+    })
+    .createTable('steps', table=> {
+        table.increments('step_id')
+        table.string('step_text', 200)
+            .notNullable()
+        table.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+    })
+    .createTable('steps_ingredients', table=> {
+        table.increments('step_ingredient_id')
+        table.float('quantity').notNullable()
+        //fk's:
+        table.integer('step_id')
+            .unsigned()
+            .notNullable()
+            .references('step_id')
+            .inTable('steps')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        table.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('ingredient_id')
+            .inTable('ingredients')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('step_ingredients')
+    .dropTableIfExists('step')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes')
+};
--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 const sharedConfig = {
     client: 'sqlite3',
     migrations: {directory: './data/migrations'},
-    seeds: { directory: './data/seeds'}
+    seeds: { directory: './data/seeds'},
     //sqlite 3 stuff
     useNullAsDefault: true,
     pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
