460a8678d2ffaaf5cba1327894f6d9d325bd1add
Victoria Blancas <vikki6688@gmail.com>
2023-10-09 17:45:24 -0400

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,7 +1,11 @@
+//db wrapper
+const db = require('../../data/db-config')
 
-function getRecipeById(recipe_id){
-    //test
-    return Promise.resolve(`model: awesome recipe ${recipe_id}`)
+
+async function getRecipeById(recipe_id){
+    const recipeRows = await db('recipes as r')
+        .where('recipe_id', recipe_id)
+    return recipeRows
 }
 
 module.exports = {
